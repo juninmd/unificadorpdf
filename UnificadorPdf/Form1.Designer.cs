7d6c7c14d3a5e49e27bc2ebfcf26b8bfb26b284c
Antonio Carlos <jr_acn@yahoo.com.br>
2015-03-11 22:40:54 -0300

    Primeiro Commit

---
+++ UnificadorPdf/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace UnificadorPdf
+{
+    partial class formPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(49, 49);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(451, 102);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Adicionar Pdfs na Lista";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Limpar Lista";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(242, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Gerar Arquivo Unificado";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // formPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(539, 194);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "formPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unificador de Pdfs por Antonio Carlos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
